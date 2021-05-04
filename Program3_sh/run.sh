b919c0d643eb900a810159137262e6d55d2161e8
Jacob Cappi <58380240+JacobCappi@users.noreply.github.com>
2021-05-04 00:48:11 -0500

    Update run.sh

--- Program3_sh/run.sh
+++ Program3_sh/run.sh
@@ -1,2 +1,2 @@
 
-tar -xzf program3_sed_project.tgz && find testapp/ -type f \( -name *.h -o -name *.cpp \) | ./cappi_cs390program3.sh
+tar -xzf program3_sed_project.tgz && find testapp/ -type f \( -name *.h -o -name *.cpp \) | ./program3.sh
